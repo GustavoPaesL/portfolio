6bdfd1655ffc912e8f3f55b80b12e01ee36c987f
Gustavo Paes Leite <gupaes2014@gmail.com>
2025-01-27 18:33:15 -0300

    refactor: js code optimization

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-function goHome() {
-    var position = 0;
-    window.scrollTo({
-      top: position,
-      behavior: 'smooth'
-    });
-}
+function goTo(positionVh) {
 
-function goAboutMe() {
-    var positionVh = 80;
-    var positionPx = (positionVh * window.innerHeight) / 100;
-
-    window.scrollTo({
-      top: positionPx,
-      behavior: 'smooth'
-    });
-}
-function goSkills() {
-  var positionVh = 160;
   var positionPx = (positionVh * window.innerHeight) / 100;
 
   window.scrollTo({
@@ -24,14 +7,4 @@ function goSkills() {
     behavior: 'smooth'
   });
 }
-function goContact() {
-  var positionVh = 240;
-  var positionPx = (positionVh * window.innerHeight) / 100;
-
-  window.scrollTo({
-    top: positionPx,
-    behavior: 'smooth'
-  });
-}
-
 
